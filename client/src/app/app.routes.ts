79a34e60c8a1594d967257125b7483b25e0f9ae6
Samiya Kazi <samiya.kazi09@gmail.com>
2024-01-01 22:42:58 +0600

    fix: added redirect to router

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { TableSetterPageComponent } from './pages/table-setter-page/table-setter-page.component';
 
 export const routes: Routes = [
-  { path: 'table-setter', component: TableSetterPageComponent, pathMatch: 'full' }
+  { path: 'table-setter', component: TableSetterPageComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/table-setter'}
 ];
